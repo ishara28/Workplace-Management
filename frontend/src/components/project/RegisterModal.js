91e1831590dc81ab4c082ab4a3441f30f6698523
DARathnasiri <dilankar.17@cse.mrt.ac.lk>
2020-08-19 18:36:20 +0530

    site register done

--- frontend/src/components/project/RegisterModal.js
+++ frontend/src/components/project/RegisterModal.js
@@ -44,20 +44,20 @@ export class RegisterModal extends Component {
     super(props);
 
     this.state = {
-      customers: [],
-      workhouses: [],
+      clients: [],
+      sites: [],
       agreements: [],
       index_no: "",
       reg_date: currDate,
+      agreement_id: "",
       owner_id: "",
+      agent_id: "",
+      site_id: "",
       description: "",
       estimated_start: "",
       estimated_days: "",
       estimated_value: "",
       status: "PROPOSED",
-      workhouse_id: "",
-      agent_id: "",
-      agreement_id: "",
       isFieldsEmpty: false,
       
     };
@@ -65,13 +65,13 @@ export class RegisterModal extends Component {
 
   componentDidMount() {
     Axios.get("/customer/")
-      .then((res) => this.setState({ customers: res.data }))
-      .then(() => console.log(this.state.customers))
+      .then((res) => this.setState({ clients: res.data }))
+      .then(() => console.log(this.state.clients))
       .catch((err) => console.log(err));
 
     Axios.get("/workhouse/")
-    .then((res) => this.setState({ workhouses: res.data }))
-    .then(() => console.log(this.state.workhouses))
+    .then((res) => this.setState({ sites: res.data }))
+    .then(() => console.log(this.state.sites))
     .catch((err) => console.log(err));
 
     Axios.get("/agreement/")
@@ -106,27 +106,26 @@ export class RegisterModal extends Component {
         const newProject = {
           index_no: this.state.index_no,
           reg_date: this.state.reg_date,
+          agreement_id: this.state.agreement_id,
           owner_id: this.state.owner_id,
+          agent_id: this.state.agent_id,
+          site_id: this.state.site_id,
           description: this.state.description,
           estimated_start: this.state.estimated_start,
           estimated_days: this.state.estimated_days,
           estimated_value: this.state.estimated_value,
           status: this.state.status,
-          workhouse_id: this.state.workhouse_id,
-          agent_id: this.state.agent_id,
-          agreement_id: this.state.agreement_id,
         };
         if (
+          this.state.agreement_id &&
           this.state.owner_id &&
+          this.state.agent_id &&
+          this.state.site_id &&
           this.state.description &&
           this.state.estimated_start &&
           this.state.estimated_days &&
-          this.state.estimated_value &&
-          this.state.workhouse_id &&
-          this.state.agent_id &&
-          this.state.agreement_id
+          this.state.estimated_value
         ) {
-          console.log(newProject);
           Axios.post("project/register", newProject)
             .then((res) => console.log(res.data))
             .then(() => {
@@ -163,8 +162,34 @@ export class RegisterModal extends Component {
           <ModalBody>
             <Form>
               <FormGroup row>
+                <Label for="agreement_id" sm={2}>
+                  Agreement
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    type="select"
+                    name="agreement_id"
+                    id="agreement_id"
+                    value={this.state.agreement_id}
+                    onChange={(e) =>
+                      this.setState({ agreement_id: e.target.value })
+                    }
+                  >
+                    <option>Choose Agreement</option>
+                    {this.state.agreements.map((agreement) => {
+                      return (
+                        <option value={agreement.a_id}>
+                          {agreement.index_no}
+                        </option>
+                      );
+                    })}
+                  </Input>
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
                 <Label for="owner_id" sm={2}>
-                  Owner
+                  Client
                 </Label>
                 <Col sm={10}>
                   <Input
@@ -177,10 +202,62 @@ export class RegisterModal extends Component {
                     }
                   >
                     <option>Choose Owner</option>
-                    {this.state.customers.map((customer) => {
+                    {this.state.clients.map((customer) => {
                       return (
                         <option value={customer.c_id}>
                           {customer.index_no + " - " + customer.name}
+                        </option>
+                      );
+                    })}
+                  </Input>
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label for="owner_id" sm={2}>
+                  Agent
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    type="select"
+                    name="agent_id"
+                    id="agent_id"
+                    value={this.state.agent_id}
+                    onChange={(e) =>
+                      this.setState({ agent_id: e.target.value })
+                    }
+                  >
+                    <option>Choose Agent</option>
+                    {this.state.clients.map((agent) => {
+                      return (
+                        <option value={agent.c_id}>
+                          {agent.index_no + " - " + agent.name}
+                        </option>
+                      );
+                    })}
+                  </Input>
+                </Col>
+              </FormGroup>
+
+              <FormGroup row>
+                <Label for="workhouse_id" sm={2}>
+                  Site
+                </Label>
+                <Col sm={10}>
+                  <Input
+                    type="select"
+                    name="workhouse_id"
+                    id="workhouse_id"
+                    value={this.state.site_id}
+                    onChange={(e) =>
+                      this.setState({ site_id: e.target.value })
+                    }
+                  >
+                    <option>Choose Site</option>
+                    {this.state.sites.map((workhouse) => {
+                      return (
+                        <option value={workhouse.w_id}>
+                          {workhouse.index_no}
                         </option>
                       );
                     })}
@@ -249,85 +326,6 @@ export class RegisterModal extends Component {
                   />
                 </Col>
               </FormGroup>
-
-              <FormGroup row>
-                <Label for="workhouse_id" sm={2}>
-                  Workhouse
-                </Label>
-                <Col sm={10}>
-                  <Input
-                    type="select"
-                    name="workhouse_id"
-                    id="workhouse_id"
-                    value={this.state.workhouse_id}
-                    onChange={(e) =>
-                      this.setState({ workhouse_id: e.target.value })
-                    }
-                  >
-                    <option>Choose Workhouse</option>
-                    {this.state.workhouses.map((workhouse) => {
-                      return (
-                        <option value={workhouse.w_id}>
-                          {workhouse.index_no}
-                        </option>
-                      );
-                    })}
-                  </Input>
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Label for="owner_id" sm={2}>
-                  Agent
-                </Label>
-                <Col sm={10}>
-                  <Input
-                    type="select"
-                    name="agent_id"
-                    id="agent_id"
-                    value={this.state.agent_id}
-                    onChange={(e) =>
-                      this.setState({ agent_id: e.target.value })
-                    }
-                  >
-                    <option>Choose Agent</option>
-                    {this.state.customers.map((agent) => {
-                      return (
-                        <option value={agent.c_id}>
-                          {agent.index_no + " - " + agent.name}
-                        </option>
-                      );
-                    })}
-                  </Input>
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Label for="agreement_id" sm={2}>
-                  Agreement
-                </Label>
-                <Col sm={10}>
-                  <Input
-                    type="select"
-                    name="agreement_id"
-                    id="agreement_id"
-                    value={this.state.agreement_id}
-                    onChange={(e) =>
-                      this.setState({ agreement_id: e.target.value })
-                    }
-                  >
-                    <option>Choose Agreement</option>
-                    {this.state.agreements.map((agreement) => {
-                      return (
-                        <option value={agreement.a_id}>
-                          {agreement.index_no}
-                        </option>
-                      );
-                    })}
-                  </Input>
-                </Col>
-              </FormGroup>
-              
             </Form>
 
             {this.state.isFieldsEmpty && (
