cf3afa1f9f6fe43c3d2753694b8871fe7afb1d2c
DARathnasiri <dilankar.17@cse.mrt.ac.lk>
2020-08-21 10:26:34 +0530

    db updated

---
+++ backend/routes/agent.js
@@ -0,0 +1,170 @@
+const router = require("express").Router();
+const mySqlConnection = require("../dbconnection");
+
+// Get all Clients
+router.get("/", (req, res) => {
+  if (req.session.isLogged) {
+    let sql = "SELECT * FROM agent";
+    let query = mySqlConnection.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+      // res.json(result);
+    });
+  } else {
+    res.send("Login First!");
+  }
+});
+
+//Get all the blocked agent
+router.get("/blocked", (req, res) => {
+  if (req.session.isLogged) {
+    let sql = "SELECT * FROM blocked_customer";
+    let query = mySqlConnection.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+      // res.json(result);
+    });
+  } else {
+    res.send("Login First!");
+  }
+});
+
+// Get a single customer
+router.get("/:c_id", (req, res) => {
+  if (req.session.isLogged) {
+    let sql = "SELECT * FROM `customer` WHERE c_id = ? ";
+    let query = mySqlConnection.query(sql, req.params.c_id, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+    });
+  } else {
+    res.send("Login First!");
+  }
+});
+
+//Active existing customer (Change status to INACTIVE)
+router.post("/inactive/:c_id", (req, res) => {
+  if (req.session.isLogged) {
+    let sql = "UPDATE customer SET status = 'INACTIVE' WHERE c_id = ?";
+    let query = mySqlConnection.query(sql, req.params.c_id, (err, result) => {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+      res.send("INACTIVE");
+    });
+  } else {
+    res.send("Login First!");
+  }
+});
+
+//Inactive existing customer (Change status to ACTIVE)
+router.post("/active/:c_id", (req, res) => {
+  if (req.session.isLogged) {
+    let sql = "UPDATE customer SET status = 'ACTIVE' WHERE c_id = ?";
+    let query = mySqlConnection.query(sql, req.params.c_id, (err, result) => {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+      res.send("ACTIVE");
+    });
+  } else {
+    res.send("Login First!");
+  }
+});
+
+//Remove existing customer (Change status to REMOVED)
+router.post("/remove/:c_id", (req, res) => {
+  if (req.session.isLogged) {
+    let sql = "UPDATE customer SET status = 'REMOVED' WHERE c_id = ?";
+    let query = mySqlConnection.query(sql, req.params.c_id, (err, result) => {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+      res.send("REMOVED");
+    });
+  } else {
+    res.send("Login First!");
+  }
+});
+
+//Register a customer
+router.post("/register", (req, res) => {
+  if (req.session.isLogged) {
+    let customer = {
+      index_no: req.body.index_no,
+      name: req.body.name,
+      nic_passport: req.body.nic_passport,
+      address: req.body.address,
+      telephone: req.body.telephone,
+      email: req.body.email,
+      description: req.body.description,
+      reg_date: req.body.reg_date,
+      status: req.body.status,
+    };
+
+    let sql = "INSERT INTO customer SET ?";
+
+    let query = mySqlConnection.query(sql, customer, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send("Customer added!");
+    });
+  } else {
+    res.send("Login First!");
+  }
+});
+
+//Update a customer
+router.post("/update/:c_id", (req, res) => {
+  if (req.session.isLogged) {
+    let customer = {
+      nic_passport: req.body.nic_passport,
+      name: req.body.name,
+      reg_date: req.body.reg_date,
+      description: req.body.description,
+      address: req.body.address,
+      telephone: req.body.telephone,
+      email: req.body.email,
+    };
+    let sql = "UPDATE customer SET ? WHERE c_id = ?";
+    let query = mySqlConnection.query(
+      sql,
+      [customer, req.params.c_id],
+      (err, result) => {
+        console.log("Customer Updated");
+        res.send("Customer Updated");
+      }
+    );
+  } else {
+    res.send("Login First!");
+  }
+});
+
+//Block an existing customer
+router.post("/block/:c_id", (req, res) => {
+  if (req.session.isLogged) {
+    let blockedCustomer = {
+      c_id: req.params.c_id,
+      blocked_date: req.body.blocked_date,
+      reason: req.body.reason,
+    };
+
+    let sql =
+      "UPDATE customer SET status = 'BLOCKED' WHERE c_id = ? ; INSERT INTO blocked_customer SET ?";
+
+    let query = mySqlConnection.query(
+      sql,
+      [req.params.c_id, blockedCustomer],
+      (err, result) => {
+        if (err) throw err;
+        console.log("User Blocked");
+        res.send("User Blocked");
+      }
+    );
+  } else {
+    res.send("Login First!");
+  }
+});
+
+
+module.exports = router;
--- database.sql
+++ database.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 15, 2020 at 09:03 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- Host: localhost
+-- Generation Time: Aug 21, 2020 at 06:55 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agent`
+--
+
+CREATE TABLE `agent` (
+  `ag_id` int(11) NOT NULL,
+  `index_no` varchar(20) NOT NULL,
+  `nic_passport` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `reg_date` varchar(10) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `status` enum('ACTIVE','INACTIVE','REMOVED','BLOCKED') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,12 +58,18 @@ CREATE TABLE `agreement` (
   `customer_index_no` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `agreement`
+-- Table structure for table `blcked_agent`
 --
 
-INSERT INTO `agreement` (`a_id`, `index_no`, `reg_date`, `reg_id`, `description`, `start_date`, `end_date`, `status`, `customer_index_no`) VALUES
-(18, 'A-2082-192249', '2020-08-02', '32413', 'fdasdasd', '2020-08-07', '2020-08-27', 'ACTIVE', 'C-2082-192224');
+CREATE TABLE `blcked_agent` (
+  `block_id` int(11) NOT NULL,
+  `ag_id` int(11) NOT NULL,
+  `blocked_date` varchar(10) NOT NULL,
+  `reason` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -62,12 +86,13 @@ CREATE TABLE `blocked_agreement` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `blocked_customer`
+-- Table structure for table `blocked_client`
 --
 
-CREATE TABLE `blocked_customer` (
+CREATE TABLE `blocked_client` (
+  `block_id` int(11) NOT NULL,
   `c_id` int(11) NOT NULL,
-  `blocked_date` date NOT NULL,
+  `blocked_date` varchar(10) NOT NULL,
   `reason` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -98,6 +123,19 @@ CREATE TABLE `blocked_organization` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `blocked_worker`
+--
+
+CREATE TABLE `blocked_worker` (
+  `block_id` int(11) NOT NULL,
+  `w_id` int(11) NOT NULL,
+  `blocked_date` varchar(10) NOT NULL,
+  `reason` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `blocked_workhouse`
 --
 
@@ -110,28 +148,28 @@ CREATE TABLE `blocked_workhouse` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer`
+-- Table structure for table `client`
 --
 
-CREATE TABLE `customer` (
+CREATE TABLE `client` (
   `c_id` int(11) NOT NULL,
   `index_no` varchar(20) NOT NULL,
-  `name` varchar(50) NOT NULL,
   `nic_passport` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `reg_date` varchar(10) NOT NULL,
+  `description` varchar(500) NOT NULL,
   `address` varchar(100) NOT NULL,
   `telephone` varchar(15) NOT NULL,
   `email` varchar(25) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `reg_date` date NOT NULL,
-  `status` enum('ACTIVE','REMOVED','BLOCKED') NOT NULL DEFAULT 'ACTIVE'
+  `status` enum('ACTIVE','INACTIVE','REMOVED','BLOCKED') NOT NULL DEFAULT 'ACTIVE'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `customer`
+-- Dumping data for table `client`
 --
 
-INSERT INTO `customer` (`c_id`, `index_no`, `name`, `nic_passport`, `address`, `telephone`, `email`, `description`, `reg_date`, `status`) VALUES
-(12, 'C-2082-192224', 'hfhkhg', '6164', 'dfsvsd', '324234', 'dsfs@efe', 'asdfaf', '2020-08-02', 'ACTIVE');
+INSERT INTO `client` (`c_id`, `index_no`, `nic_passport`, `name`, `reg_date`, `description`, `address`, `telephone`, `email`, `status`) VALUES
+(17, 'C-20820-74655', '1111111', 'saman', '2020-08-25', 'saman', 'matara', '1111111', 'saman@gmail.com', 'ACTIVE');
 
 -- --------------------------------------------------------
 
@@ -149,6 +187,13 @@ CREATE TABLE `machinery` (
   `description` varchar(500) NOT NULL,
   `owner_index_no` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `machinery`
+--
+
+INSERT INTO `machinery` (`m_id`, `index_no`, `reg_id`, `reg_date`, `status`, `category`, `description`, `owner_index_no`) VALUES
+(47, 'M-20820-870', '1111111', '2020-08-20', 'ACTIVE', 'VEHICLE', '1111111', 'C-20820-74655');
 
 -- --------------------------------------------------------
 
@@ -168,6 +213,13 @@ CREATE TABLE `organization` (
   `reg_id` varchar(20) NOT NULL,
   `c_id` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `organization`
+--
+
+INSERT INTO `organization` (`o_id`, `index_no`, `reg_date`, `status`, `address`, `telephone`, `email`, `description`, `reg_id`, `c_id`) VALUES
+(7, 'O-20817-10326', '2020-08-17', 'ACTIVE', '234124242', '34545', '3453414', '234242', '32424', '12');
 
 -- --------------------------------------------------------
 
@@ -195,7 +247,32 @@ CREATE TABLE `project` (
 --
 
 INSERT INTO `project` (`p_id`, `owner_id`, `index_no`, `reg_date`, `description`, `estimated_start`, `estimated_days`, `estimated_value`, `status`, `workhouse_id`, `agent_id`, `agreement_id`) VALUES
-(5, 12, 'P-2082-192', '2020-08-02', 'DSAF', '2020-08-20', 2, '0.00', 'PROPOSED', 5, 12, 18);
+(5, 13, 'P-2082-192', '2020-08-02', 'DSAFs', '2020-08-28', 24, '2.00', 'PROPOSED', 5, 12, 18);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site`
+--
+
+CREATE TABLE `site` (
+  `s_id` int(11) NOT NULL,
+  `index_no` varchar(20) NOT NULL,
+  `reg_date` date NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `site`
+--
+
+INSERT INTO `site` (`s_id`, `index_no`, `reg_date`, `description`, `address`, `telephone`, `email`, `c_id`, `status`) VALUES
+(5, 'W-2082-192343', '2020-08-02', 'SADAD', 'SDFAF', '11111', 'ASDFA@DSF', 12, 'ACTIVE');
 
 -- --------------------------------------------------------
 
@@ -219,31 +296,33 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `workhouse`
+-- Table structure for table `worker`
 --
 
-CREATE TABLE `workhouse` (
+CREATE TABLE `worker` (
   `w_id` int(11) NOT NULL,
   `index_no` varchar(20) NOT NULL,
-  `reg_date` date NOT NULL,
-  `status` varchar(10) NOT NULL,
+  `nic_passport` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `reg_date` varchar(10) NOT NULL,
+  `site` int(11) NOT NULL,
+  `description` varchar(500) NOT NULL,
   `address` varchar(100) NOT NULL,
   `telephone` varchar(15) NOT NULL,
   `email` varchar(25) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `c_id` varchar(30) NOT NULL
+  `status` enum('ACTIVE','INACTIVE','REMOVED','BLOCKED') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `workhouse`
---
-
-INSERT INTO `workhouse` (`w_id`, `index_no`, `reg_date`, `status`, `address`, `telephone`, `email`, `description`, `c_id`) VALUES
-(5, 'W-2082-192343', '2020-08-02', 'ACTIVE', 'SDFAF', 'SDAFA', 'ASDFA@DSF', 'SADAD', '12');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `agent`
+--
+ALTER TABLE `agent`
+  ADD PRIMARY KEY (`ag_id`),
+  ADD KEY `index_no` (`index_no`);
 
 --
 -- Indexes for table `agreement`
@@ -253,16 +332,24 @@ ALTER TABLE `agreement`
   ADD KEY `customer_index_no` (`customer_index_no`);
 
 --
+-- Indexes for table `blcked_agent`
+--
+ALTER TABLE `blcked_agent`
+  ADD PRIMARY KEY (`block_id`),
+  ADD KEY `ag_id` (`ag_id`);
+
+--
 -- Indexes for table `blocked_agreement`
 --
 ALTER TABLE `blocked_agreement`
   ADD PRIMARY KEY (`a_id`);
 
 --
--- Indexes for table `blocked_customer`
+-- Indexes for table `blocked_client`
 --
-ALTER TABLE `blocked_customer`
-  ADD PRIMARY KEY (`c_id`);
+ALTER TABLE `blocked_client`
+  ADD PRIMARY KEY (`block_id`),
+  ADD KEY `c_id` (`c_id`);
 
 --
 -- Indexes for table `blocked_machinery`
@@ -277,15 +364,22 @@ ALTER TABLE `blocked_organization`
   ADD PRIMARY KEY (`o_id`);
 
 --
+-- Indexes for table `blocked_worker`
+--
+ALTER TABLE `blocked_worker`
+  ADD PRIMARY KEY (`block_id`),
+  ADD KEY `w_id` (`w_id`);
+
+--
 -- Indexes for table `blocked_workhouse`
 --
 ALTER TABLE `blocked_workhouse`
   ADD PRIMARY KEY (`w_id`);
 
 --
--- Indexes for table `customer`
+-- Indexes for table `client`
 --
-ALTER TABLE `customer`
+ALTER TABLE `client`
   ADD PRIMARY KEY (`c_id`,`index_no`),
   ADD KEY `index_no` (`index_no`);
 
@@ -310,21 +404,34 @@ ALTER TABLE `project`
   ADD PRIMARY KEY (`p_id`);
 
 --
+-- Indexes for table `site`
+--
+ALTER TABLE `site`
+  ADD PRIMARY KEY (`s_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `workhouse`
+-- Indexes for table `worker`
 --
-ALTER TABLE `workhouse`
+ALTER TABLE `worker`
   ADD PRIMARY KEY (`w_id`),
-  ADD KEY `c_id` (`c_id`);
+  ADD KEY `index_no` (`index_no`),
+  ADD KEY `site` (`site`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `agent`
+--
+ALTER TABLE `agent`
+  MODIFY `ag_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `agreement`
@@ -333,10 +440,16 @@ ALTER TABLE `agreement`
   MODIFY `a_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `blocked_customer`
+-- AUTO_INCREMENT for table `blcked_agent`
 --
-ALTER TABLE `blocked_customer`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `blcked_agent`
+  MODIFY `block_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `blocked_client`
+--
+ALTER TABLE `blocked_client`
+  MODIFY `block_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `blocked_machinery`
@@ -351,28 +464,34 @@ ALTER TABLE `blocked_organization`
   MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `blocked_worker`
+--
+ALTER TABLE `blocked_worker`
+  MODIFY `block_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `blocked_workhouse`
 --
 ALTER TABLE `blocked_workhouse`
   MODIFY `w_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=222223;
 
 --
--- AUTO_INCREMENT for table `customer`
+-- AUTO_INCREMENT for table `client`
 --
-ALTER TABLE `customer`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+ALTER TABLE `client`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `machinery`
 --
 ALTER TABLE `machinery`
-  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `organization`
 --
 ALTER TABLE `organization`
-  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `project`
@@ -381,16 +500,22 @@ ALTER TABLE `project`
   MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `site`
+--
+ALTER TABLE `site`
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `workhouse`
+-- AUTO_INCREMENT for table `worker`
 --
-ALTER TABLE `workhouse`
-  MODIFY `w_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `worker`
+  MODIFY `w_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -400,7 +525,13 @@ ALTER TABLE `workhouse`
 -- Constraints for table `agreement`
 --
 ALTER TABLE `agreement`
-  ADD CONSTRAINT `agreement_ibfk_1` FOREIGN KEY (`customer_index_no`) REFERENCES `customer` (`index_no`) ON DELETE CASCADE;
+  ADD CONSTRAINT `agreement_ibfk_1` FOREIGN KEY (`customer_index_no`) REFERENCES `client` (`index_no`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `blcked_agent`
+--
+ALTER TABLE `blcked_agent`
+  ADD CONSTRAINT `blcked_agent_ibfk_1` FOREIGN KEY (`ag_id`) REFERENCES `agent` (`ag_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `blocked_agreement`
@@ -409,10 +540,10 @@ ALTER TABLE `blocked_agreement`
   ADD CONSTRAINT `blocked_agreement_ibfk_1` FOREIGN KEY (`a_id`) REFERENCES `agreement` (`a_id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `blocked_customer`
+-- Constraints for table `blocked_client`
 --
-ALTER TABLE `blocked_customer`
-  ADD CONSTRAINT `blocked_customer_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`) ON DELETE CASCADE;
+ALTER TABLE `blocked_client`
+  ADD CONSTRAINT `blocked_client_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `client` (`c_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `blocked_machinery`
@@ -427,10 +558,22 @@ ALTER TABLE `blocked_organization`
   ADD CONSTRAINT `blocked_organization_ibfk_1` FOREIGN KEY (`o_id`) REFERENCES `organization` (`o_id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `blocked_worker`
+--
+ALTER TABLE `blocked_worker`
+  ADD CONSTRAINT `blocked_worker_ibfk_1` FOREIGN KEY (`w_id`) REFERENCES `worker` (`w_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `machinery`
 --
 ALTER TABLE `machinery`
-  ADD CONSTRAINT `machinery_ibfk_1` FOREIGN KEY (`owner_index_no`) REFERENCES `customer` (`index_no`);
+  ADD CONSTRAINT `machinery_ibfk_1` FOREIGN KEY (`owner_index_no`) REFERENCES `client` (`index_no`);
+
+--
+-- Constraints for table `worker`
+--
+ALTER TABLE `worker`
+  ADD CONSTRAINT `worker_ibfk_1` FOREIGN KEY (`site`) REFERENCES `site` (`s_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
